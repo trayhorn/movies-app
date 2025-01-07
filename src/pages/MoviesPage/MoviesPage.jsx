6ec28d830e474ea8652ee26b0adaf7fbb5ab9f66
Ivan Amoshyi <amoshyiivan@gmail.com>
2025-01-07 18:20:58 +0200

    added useSearchParams in MovieDetailsPage

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -7,13 +7,20 @@ export default function MoviesPage() {
   const [searchedMovies, setSearchedMovies] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const searchQuery = searchParams.get('query') ?? '';
+	const searchQuery = searchParams.get('query') ?? '';
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     setQuery(e.target.elements.search.value);
-  }
+	}
+
+	const updateQuery = (e) => {
+		const query = e.target.value;
+		query !== ''
+			? setSearchParams({ query })
+			: setSearchParams({})
+	}
 
   useEffect(() => {
     if (query === '') {
@@ -42,7 +49,7 @@ export default function MoviesPage() {
 					name="search"
 					id="input1"
 					value={searchQuery}
-					onChange={(e) => setSearchParams({ query: e.target.value })}
+					onChange={updateQuery}
 				/>
 				<button type="submit">Search</button>
 			</form>
