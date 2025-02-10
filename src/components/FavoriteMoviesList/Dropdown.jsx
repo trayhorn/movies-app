6dc1c4afdfcd388ff53fc9666d6f291f6e2525b7
Ivan Amoshyi <amoshyiivan@gmail.com>
2025-02-10 17:55:55 +0200

    styled dropdown

--- src/components/FavoriteMoviesList/Dropdown.jsx
+++ src/components/FavoriteMoviesList/Dropdown.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import "./Dropdown.scss";
 import PropTypes from "prop-types";
+import { BsThreeDotsVertical } from "react-icons/bs";
 
 export default function Dropdown({movie, lists}) {
   const [openId, setOpenId] = useState(false);
@@ -21,19 +22,24 @@ export default function Dropdown({movie, lists}) {
 
   return (
 		<div className="addToListWrapper" ref={ref}>
-			<button
+			{/* <button
 				className="add-button"
-        onClick={() =>
-          setOpenId(movie.id === openId ? null : movie.id)
-        }
-			>
-				Add
-			</button>
+				onClick={() => setOpenId(movie.id === openId ? null : movie.id)}
+			> */}
+			<div className="add-icon_container">
+				<BsThreeDotsVertical
+					className="add-icon"
+					size="1.5rem"
+					// color="#FFFF"
+					onClick={() => setOpenId(movie.id === openId ? null : movie.id)}
+				/>
+			</div>
+			{/* </button> */}
 			<div className="addToList">
 				{openId === movie.id && (
 					<ul className="dropdown">
 						{lists.map((el) => {
-							return <li key={el.id}>{el.name}</li>;
+							return <li className="dropdown-item" key={el.id}>{el.name}</li>;
 						})}
 					</ul>
 				)}
